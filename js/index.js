0489e7fbfa7fe24b222db504c44624542b79127f
Ali123-gig <ali99shair@gmail.com>
2020-06-15 12:14:58 +0500

    did changes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,27 @@
 var auth=firebase.auth();
 var firestore=firebase.firestore();
-console.log(auth)
-console.log(firestore)
+
+var signInFormSubmission=(e)=>{
+e.preventDefault();
+var email=document.querySelector(".signIn-email").value
+console.log(email)
+var password=document.querySelector(".signIn-password").value
+console.log(password)
+
+
+}
+
+var signupFormSubmission=(e)=>{
+ e.preventDefault();
+var fullname=document.querySelector(".signUp-name").value
+console.log(fullname)
+ var email=document.querySelector(".signUp-email").value
+ console.log(email)
+var password=document.querySelector(".signUp-password").value
+console.log(password)
+    }
+
+var siginForm =document.querySelector(".siginForm ");
+siginForm.addEventListener("submit",(e)=>signInFormSubmission(e));
+var signupForm =document.querySelector(".signupForm ")
+signupForm.addEventListener("submit",(e)=>signupFormSubmission(e));
